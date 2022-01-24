ec8b778a30b8208bb99744add211fc79c05eb853
gargjagri <98157261+gargjagri@users.noreply.github.com>
2022-01-24 18:51:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const republic = document.getElementById("republic");
+
+republic.onclick=()=>{
+    //action 1
+    location.href="./republic.html"
+    //action2
+    var audio = new Audio("https://upload.wikimedia.org/wikipedia/commons/9/94/Jana_Gana_Mana_instrumental.ogg");
+    audio.play();
+}
+
+
+
+
